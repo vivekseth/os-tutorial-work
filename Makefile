81af2ab407a333f3e7eb81f92a708a58e141fa56
Vivek Seth <Vivekseth.m@gmail.com>
2018-03-03 20:54:38 -0800

    Simple boot example

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 
 simple_boot.bin: simple_boot.asm
 	nasm -f bin simple_boot.asm -o simple_boot.bin
+
+run: simple_boot.bin
+	qemu-system-x86_64 ./simple_boot.bin
+
+clean:
+	rm simple_boot.bin
--- simple_boot.asm
+++ simple_boot.asm
@@ -1,6 +1,21 @@
-; Infinite loop
-loop: 
-  jmp loop
+_start:
+    mov ah, 0x0e ; tty mode
+    mov al, 'H'
+    int 0x10
+    mov al, 'e'
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    int 0x10 ; 'l' is still on al, remember?
+    mov al, 'o'
+    int 0x10
+    call test
+
+test:
+    jmp $
+    ret    
+
+; jmp $ ; jump to current address = infinite loop
 
 ; Fill bytes up to (and including) 510 with zero
 times 510-($-$$) db 0
