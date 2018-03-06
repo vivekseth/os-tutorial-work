4333937f982e2da8cbe81f9e0a1e6b2b2ac60934
Vivek Seth <Vivekseth.m@gmail.com>
2018-03-05 20:13:44 -0800

    Print every foreground, background color combination

--- kernel.c
+++ kernel.c
@@ -1,7 +1,53 @@
 // void dummy_entrypoint(void) {
 // }
 
+static char *VIDEO_MEMORY;
+
+void set_char_at(char c, char format, int x, int y) {
+  char *pixel = VIDEO_MEMORY + 2 * (80 * y + x);
+  pixel[0] = c;
+  pixel[1] = format;
+}
+
+char solid_color_format(int code) {
+  // code = code & 0xF;
+  char format = (code << 4) + code;
+  return format;
+}
+
+char color_format(char fc, char bc) {
+  return ((fc & 0xF) << 4) + (bc & 0xF);
+}
+
+void clear_screen() {
+  int code = 0;
+
+  for (int x=0; x<80; x++) {
+    for (int y=0; y<25; y++) {
+      char f = code / 16;
+      char b = code % 16;
+      set_char_at('X', color_format(f, b), x, y);
+      code = (code + 1) % (16 * 16);
+    }
+  }
+}
+
+
+
+
 void _start() {
-  char *video_memory = (char *)0xb8000;
-  *video_memory = 'X';
+  VIDEO_MEMORY = (char *)0xb8000;
+
+  clear_screen();
+
+  // char *video_memory = (char *)0xb8000;
+  // *VIDEO_MEMORY = 'X';
+  // *VIDEO_MEMORY
+  // *(VIDEO_MEMORY + 2) = '@';
+
+  // for (int i=0; i<80; i++) {
+  //   int x = i * 25 / 80;
+  //   int y = i;
+  //   set_char_at('X', 0xEE, y, x);  
+  // }
 }
