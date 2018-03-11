e19bce46dbda0180389a37726e5208b74e13b821
Vivek Seth <Vivekseth.m@gmail.com>
2018-03-11 15:45:41 -0700

    Add "standard" functions for port communication

--- drivers/ports.c
+++ drivers/ports.c
@@ -23,3 +23,16 @@ void port_write_word(unsigned short port, unsigned char data) {
   // out dx, ax
   asm("out %%dx, %%ax" : : "a" (data), "d" (port));
 }
+
+unsigned char port_byte_in(unsigned short port) {
+  return port_read_byte(port);
+}
+void port_byte_out(unsigned short port, unsigned char data) {
+  return port_write_byte(port, data);
+}
+unsigned short port_word_in(unsigned short port) {
+  return port_read_word(port);
+}
+void port_word_out(unsigned short port, unsigned char data) {
+  return port_write_word(port, data);
+}
--- drivers/ports.h
+++ drivers/ports.h
@@ -1,3 +1,9 @@
+
+unsigned char port_byte_in(unsigned short port);
+void port_byte_out(unsigned short port, unsigned char data);
+unsigned short port_word_in(unsigned short port);
+void port_word_out(unsigned short port, unsigned char data);
+
 unsigned char port_read_byte(unsigned short port);
 void port_write_byte(unsigned short port, unsigned char data);
 unsigned short port_read_word(unsigned short port);
